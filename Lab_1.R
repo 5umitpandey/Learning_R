98f8d39bcd53366968f0a068b677b8a2b0ee9aee
Sumit Pandey <68957789+5umitpandey@users.noreply.github.com>
2025-01-20 02:28:03 +0530

    Add files via upload

---
+++ Lab_1.R
@@ -0,0 +1,46 @@
+#1
+A <- 23.4
+B <- 45
+C <- 678
+cat("A =", A, "B =", B, "C =", C, "\n")
+
+
+#2
+ls()
+
+
+#3
+rm(C)
+ls()
+
+
+#4
+#I am learning R
+
+
+#5
+firstname <- "MyName"; lastname <- "MuSurname"
+
+
+#6
+x <- 1
+
+
+#7
+C <- 678
+print(A + B - C)
+
+
+#8
+print(exp(4))
+print(log(10))
+print(log10(10))
+print(pi)
+print(sqrt(49))
+
+
+#9
+print( 23 + (4.5 * 2.3) / 10 )
+print( 456 / 12 - log(90) )
+print( exp(5) + 12 / (5^6) )
+print( sqrt(45) * 12/3 )
